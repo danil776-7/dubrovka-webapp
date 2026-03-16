49fcbdd7b3580c55d0d3cca4ac68b3d8ef4dcac0
danil776-7 <144059555+danil776-7@users.noreply.github.com>
2026-03-16 14:00:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let selectedTable = null
 
+const tableDescriptions = {
+
+1:"Приватная лаунж-зона со шторками и PlayStation",
+2:"Приватная лаунж-зона со шторками и PlayStation",
+3:"Приватная лаунж-зона со шторками и PlayStation",
+4:"Приватная лаунж-зона со шторками и PlayStation",
+
+5:"Открытая лаунж-зона без PlayStation",
+
+6:"Компактный стол для 2-3 гостей",
+
+VIP:"VIP комната. Бронирование через администратора"
+
+}
+
 document.querySelectorAll(".table").forEach(table=>{
 
 table.addEventListener("click",()=>{
@@ -9,6 +24,9 @@ document.querySelectorAll(".table").forEach(t=>t.classList.remove("selected"))
 table.classList.add("selected")
 
 selectedTable = table.dataset.table
+
+document.getElementById("tableInfo").innerText =
+tableDescriptions[selectedTable]
 
 })
 
@@ -22,11 +40,13 @@ let name = document.getElementById("name").value
 let phone = document.getElementById("phone").value
 
 let data = {
+
 date:date,
 time:time,
 table:selectedTable,
 name:name,
 phone:phone
+
 }
 
 let tg = window.Telegram.WebApp
